a1220d7dd24443c9521ab69b1472d3025c92076e
dshmmd <mohammadzarea.bidaki@gmail.com>
2024-12-30 14:59:30 +0330

    add api prefix but seperate admin panel

--- app/urls.py
+++ app/urls.py
@@ -34,11 +34,16 @@ schema_view = get_schema_view(
     public=True,
 )
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+api_urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # Swagger UI
     path('account/', include('account.urls')),
     path('advertisement/', include("advertisement.urls"))
 ]
 
+urlpatterns = [
+    path('api/', include(api_urlpatterns)),
+    path('admin/', admin.site.urls),
+]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
